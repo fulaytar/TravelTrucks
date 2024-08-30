42fc0a120e8af12e497f8a58a289b3bb3dfe7574
fulaytar <fulaytar2405@gmail.com>
2024-08-30 22:53:53 +0300

    add hero section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,17 @@ import css from './HomePage.module.css';
 export default function HomePage() {
   return (
     <main>
-      <h1>Hello</h1>
+      <section className={css.container}>
+        <div className={css.container_info}>
+          <h1 className={css.h1}>Campers of your dreams</h1>
+          <p className={css.p}>
+            You can find everything you want in our catalog
+          </p>
+          <button type='button' className={css.btn}>
+            View Now
+          </button>
+        </div>
+      </section>
     </main>
   );
 }
